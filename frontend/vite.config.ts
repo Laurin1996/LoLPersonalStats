766f9c1f298e62625e24479fcd47adc6b0f68e8a
Lauin Off <laurin.off@gmx.de>
2022-03-19 19:02:04 +0100

    Removed vuetify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,11 @@
+import { fileURLToPath, URL } from 'url';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vuetify from '@vuetify/vite-plugin';
-import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: { 'process.env': {} },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
